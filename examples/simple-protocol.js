c3ae70ca9a01ebe450b099a16b49c5d4adb8b30c
Paolo Fragomeni <hij1nx@me.com>
2012-02-02 15:37:05 -0500

    [dist] ci support, version bump

--- examples/simple-protocol.js
+++ examples/simple-protocol.js
@@ -1,33 +1,51 @@
 var nssocket = require('../lib/nssocket');
 
 //
-// Create an `nssocket` TCP server
+// define a simple message protocol as [<type>, <id>] and create some messages that use it.
+//
+var message1 = ['message', 'one'];
+var message2 = ['message', 'two'];
+
+//
+// Create an `nssocket` TCP server and tell the server to listen on port `6785`.
 //
 var server = nssocket.createServer(function (socket) {
+
   //
   // Here `socket` will be an instance of `nssocket.NsSocket`.
+  // When there is a connection, send `message1` to the socket.
   //
-  socket.send(['you', 'there']);
-  socket.data(['iam', 'here'], function (data) {
+  socket.send(message1);
+
+  //
+  // listen for `message2` from the connecting socket.
+  //
+  socket.data(message2, function (data) {
+
     //
-    // Good! The socket speaks our language 
-    // (i.e. simple 'you::there', 'iam::here' protocol)
-    //
-    // { iam: true, indeedHere: true }
+    // If this callback is called, we know that the socket
+    // speaks our language, we will likely be provided with
+    // a payload. In this case `{ "foo": "bar" }`.
     //
     console.dir(data);
   })
-});
+
+}).listen(6785);
 
 //
-// Tell the server to listen on port `6785` and then connect to it
-// using another NsSocket instance.
+// Create a new `nssocket` instance and then connect to the server in 1000 miliseconds.
 //
-server.listen(6785);
+setTimeout(function() {
 
-var outbound = new nssocket.NsSocket();
-outbound.data(['you', 'there'], function () {
-  outbound.send(['iam', 'here'], { iam: true, indeedHere: true });
-});
+  var outbound = new nssocket.NsSocket();
 
-outbound.connect(6785);
+  //
+  // 
+  //
+  outbound.data(message1, function () {
+    outbound.send(message2, { "foo": "bar" });
+  });
+
+  outbound.connect(6785);
+  
+}, 1000);
